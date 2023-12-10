81542aea033b835ae2b67567b5848da112d9f70e
cherhy <ekxk1234@naver.com>
2023-12-10 22:30:50 +0900

    create market

---
+++ src/main/kotlin/com/example/paymentassignment/market/Market.kt
@@ -0,0 +1,8 @@
+package com.example.paymentassignment.market
+
+/**
+ * 가맹점은 id, 생성 시간( createdAt ), 수정 시간( updatedAt ), 가맹점 명, 통장( 1:N ), 가맹점 active ( deleted, deletedAt )
+ * 노출 되어도 괜찮은 식별자( 생성 날짜 + uuid 12자리 ) 를 가지고 있습니다.
+ */
+class Market {
+}
