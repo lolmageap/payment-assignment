dd47dc3992e2370db28d87aab15012ea75c85592
cherhy <ekxk1234@naver.com>
2023-12-11 23:56:30 +0900

    create payment api

--- src/main/kotlin/com/example/paymentassignment/controller/PaymentController.kt
+++ src/main/kotlin/com/example/paymentassignment/controller/PaymentController.kt
@@ -1,7 +1,13 @@
 package com.example.paymentassignment.controller
 
+import com.example.paymentassignment.bankbook.BankbookService
+import com.example.paymentassignment.customer.CustomerService
+import com.example.paymentassignment.market.MarketService
+import com.example.paymentassignment.order.OrderService
+import com.example.paymentassignment.paymenthistory.PaymentHistoryService
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 /**
  * 가맹점에서 결제 서버로 가맹점 key, List<상품, 상품 갯수>, 계좌 번호, 주문번호( 날짜 + uuid 12자리 ) 를 만들어서 보냅니다.
@@ -10,10 +16,23 @@ import org.springframework.web.bind.annotation.PostMapping
  * 동일한 결제 요청이 5초 이내로 들어오게 된다면 early return 을 발생시킵니다. ( spring data reactive redis 도입 고려 )
  * key : 주문번호 or ( 가맹점 key, 계좌번호 ), expired time : 5초
  */
-class PaymentController() {
+class PaymentController(
+    private val bankBookService: BankbookService,
+    private val customerService: CustomerService,
+    private val marketService: MarketService,
+    private val orderService: OrderService,
+    private val paymentHistoryService: PaymentHistoryService,
+) {
 
-    @PostMapping("/api/v1/market/{market-key}/order")
-    suspend fun payment(@PathVariable("market-key") marketKey: String) {
+    @PostMapping("/api/v1/market/{market-token}/order")
+    suspend fun payment(
+        @PathVariable("market-token") token: String,
+        @RequestBody paymentRequest: PaymentRequest,
+    ) {
+        val bankbook = bankBookService.getBankbookByAccountNumber(paymentRequest.accountNumber)
+        customerService.matching(bankbook)
+        val market = marketService.getMarket(token)
 
+        orderService.createOrder(bankbook, market, paymentRequest.price)
     }
 }
