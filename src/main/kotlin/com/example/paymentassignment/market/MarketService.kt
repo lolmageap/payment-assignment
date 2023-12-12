eecd34a8e1b4f9223c7701693041b5564660c32b
cherhy <ekxk1234@naver.com>
2023-12-12 22:31:12 +0900

    create receive money logic

--- src/main/kotlin/com/example/paymentassignment/market/MarketService.kt
+++ src/main/kotlin/com/example/paymentassignment/market/MarketService.kt
@@ -1,10 +1,19 @@
 package com.example.paymentassignment.market
 
+import org.springframework.stereotype.Service
+import java.math.BigDecimal
+
+@Service
 class MarketService(
     private val marketRepository: MarketRepository,
 ) {
     suspend fun getMarket(token: String) =
         marketRepository.findByToken(token)
             ?: throw IllegalAccessException()
+
+    suspend fun receiveMoney(market: Market, price: BigDecimal) {
+        market.bankbook.balance += price
+        marketRepository.save(market)
+    }
 
 }
