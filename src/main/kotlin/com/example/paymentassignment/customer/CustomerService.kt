fa90cf1473e418895092b46b455fa16a20274314
cherhy <ekxk1234@naver.com>
2023-12-12 22:30:52 +0900

    remove method

--- src/main/kotlin/com/example/paymentassignment/customer/CustomerService.kt
+++ src/main/kotlin/com/example/paymentassignment/customer/CustomerService.kt
@@ -1,15 +1,8 @@
 package com.example.paymentassignment.customer
 
-import com.example.paymentassignment.bankbook.Bankbook
-
 
 class CustomerService(
     private val customerRepository: CustomerRepository,
 ) {
-
-    suspend fun matching(bankbook: Bankbook) {
-        val exist = customerRepository.existsByBankbook(bankbook)
-        if (exist) throw IllegalArgumentException()
-    }
 
 }
