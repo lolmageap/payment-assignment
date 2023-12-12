b944bb913a0fd1244565a109f85b3157b568f6ab
cherhy <ekxk1234@naver.com>
2023-12-12 22:30:20 +0900

    create pay money logic

--- src/main/kotlin/com/example/paymentassignment/bankbook/BankbookService.kt
+++ src/main/kotlin/com/example/paymentassignment/bankbook/BankbookService.kt
@@ -1,12 +1,21 @@
 package com.example.paymentassignment.bankbook
 
+import org.springframework.stereotype.Service
 import java.lang.IllegalArgumentException
+import java.math.BigDecimal
 
+@Service
 class BankbookService(
-    private val bankBookRepository: BankbookRepository,
+    private val bankbookRepository: BankbookRepository,
 ) {
+
     suspend fun getBankbookByAccountNumber(accountNumber: String) =
-        bankBookRepository.findByAccountNumber(accountNumber)
+        bankbookRepository.findByAccountNumber(accountNumber)
             ?: throw IllegalArgumentException()
+
+    suspend fun payMoney(bankbook: Bankbook, price: BigDecimal) {
+        bankbook.balance -= price
+        bankbookRepository.save(bankbook)
+    }
 
 }
