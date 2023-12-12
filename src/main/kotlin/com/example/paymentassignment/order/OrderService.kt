f8974dca743b6b006cf416b52b419d68c0c381f8
cherhy <ekxk1234@naver.com>
2023-12-12 22:31:29 +0900

    create save order method

--- src/main/kotlin/com/example/paymentassignment/order/OrderService.kt
+++ src/main/kotlin/com/example/paymentassignment/order/OrderService.kt
@@ -2,13 +2,25 @@ package com.example.paymentassignment.order
 
 import com.example.paymentassignment.bankbook.Bankbook
 import com.example.paymentassignment.market.Market
+import org.springframework.stereotype.Service
 import java.math.BigDecimal
 
+@Service
 class OrderService(
     private val orderRepository: OrderRepository,
 ) {
-    suspend fun createOrder(bankbook: Bankbook, market: Market, price: BigDecimal) {
 
+    suspend fun saveOrder(
+        bankbook: Bankbook,
+        market: Market,
+        price: BigDecimal,
+    ) {
+        val order = Order(
+            customer = bankbook.customer,
+            market = market,
+            price = price,
+        )
+        orderRepository.save(order)
     }
 
 }
