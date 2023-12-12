d86c0bdd365e238f467b16d93ab1d1aedbf8202b
cherhy <ekxk1234@naver.com>
2023-12-12 22:32:09 +0900

    create save payment history method

--- src/main/kotlin/com/example/paymentassignment/paymenthistory/PaymentHistoryService.kt
+++ src/main/kotlin/com/example/paymentassignment/paymenthistory/PaymentHistoryService.kt
@@ -1,7 +1,26 @@
 package com.example.paymentassignment.paymenthistory
 
+import com.example.paymentassignment.bankbook.Bankbook
+import com.example.paymentassignment.market.Market
+import org.springframework.stereotype.Service
+import java.math.BigDecimal
+
+@Service
 class PaymentHistoryService(
     private val paymentHistoryRepository: PaymentHistoryRepository
 ) {
-
+    suspend fun saveHistory(
+        bankbook: Bankbook,
+        market: Market,
+        price: BigDecimal,
+        isSuccess: Boolean,
+    ) {
+        val paymentHistory = PaymentHistory(
+            customer = bankbook.customer,
+            market = market,
+            price = price,
+            isSuccess = isSuccess,
+        )
+        paymentHistoryRepository.save(paymentHistory)
+    }
 }
