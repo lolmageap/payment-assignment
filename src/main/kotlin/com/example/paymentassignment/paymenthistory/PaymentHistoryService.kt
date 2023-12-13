e4b3e791aeae086e97b462a83dda0fa93d749b22
cherhy <ekxk1234@naver.com>
2023-12-13 23:43:04 +0900

    add get methods

--- src/main/kotlin/com/example/paymentassignment/paymenthistory/PaymentHistoryService.kt
+++ src/main/kotlin/com/example/paymentassignment/paymenthistory/PaymentHistoryService.kt
@@ -1,7 +1,9 @@
 package com.example.paymentassignment.paymenthistory
 
 import com.example.paymentassignment.bankbook.Bankbook
+import com.example.paymentassignment.customer.Customer
 import com.example.paymentassignment.market.Market
+import com.example.paymentassignment.order.Order
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
 
@@ -12,15 +14,25 @@ class PaymentHistoryService(
     suspend fun saveHistory(
         bankbook: Bankbook,
         market: Market,
+        order: Order,
         price: BigDecimal,
         isSuccess: Boolean,
     ) {
         val paymentHistory = PaymentHistory(
             customer = bankbook.customer,
             market = market,
+            order = order,
             price = price,
             isSuccess = isSuccess,
         )
         paymentHistoryRepository.save(paymentHistory)
     }
+
+    fun getAll(customer: Customer) = paymentHistoryRepository.findAllByCustomer(customer)
+
+    fun isSuccessToOrder(order: Order) =
+        paymentHistoryRepository.findByOrder(order)
+            ?.isSuccess
+            ?: throw IllegalArgumentException()
+
 }
